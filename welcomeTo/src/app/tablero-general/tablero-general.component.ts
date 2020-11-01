c6e6d24172fd07d24e8cc0dc9fa334af89cfd8cd
Fuku <fukushimafederico@hotmail.com>
2020-11-01 04:28:16 -0300

    Agrego la funcion shuffle para desordenar el mazo

--- welcomeTo/src/app/tablero-general/tablero-general.component.ts
+++ welcomeTo/src/app/tablero-general/tablero-general.component.ts
@@ -25,6 +25,7 @@ export class TableroGeneralComponent implements OnInit {
     this.mazo.push(new Carta("mejorarParque",2))
     this.mazo.push(new Carta("contratarObreroTemporal",7))
     //mezclar
+    this.shuffle(this.mazo);
 
    }
 
@@ -35,5 +36,24 @@ export class TableroGeneralComponent implements OnInit {
     this.mazo.shift();
     this.mazo.shift();
     this.mazo.shift();
+  }
+
+ shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
   }
 }
